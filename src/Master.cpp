4984d96f122a70f16355cfa3490ca6565118b36c
GLeidius <gleidar5000@gmail.com>
2025-03-21 14:26:46 +0700

    add comment :D

--- src/Master.cpp
+++ src/Master.cpp
@@ -25,14 +25,14 @@ void set_power(int power){                        // функция настро
     Serial.println(pw);
 }
 
-void read_SSerial(){
+void read_SSerial(){                              // функция чтения Soft UART с задержкой
   delay(100);
   while(S_Serial.available()){
     byte buff123 = S_Serial.read();
     Serial.write(buff123);  
 }}
 
-int set_pause(int pause){
+int set_pause(int pause){                         // функция установки паузы передачи
   Serial.print("Пауза, мс: ");
   Serial.println(pause);
   int test_delay=pause-300;
